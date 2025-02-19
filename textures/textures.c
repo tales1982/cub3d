c4cfbf1a4cde1e0e1853dc05a7613844b14fb41c
Tales Lima De Paula <tlima-de@c2r4s1.42luxembourg.lu>
2025-02-19 13:30:54 +0100

    norminette textura e utils

--- textures/textures.c
+++ textures/textures.c
@@ -3,85 +3,100 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tales <tales@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tlima-de <tlima-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:19:07 by tlima-de          #+#    #+#             */
-/*   Updated: 2025/02/17 15:26:29 by tales            ###   ########.fr       */
+/*   Updated: 2025/02/19 13:17:55 by tlima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "textures.h"
 #include <stdio.h>
 
-void load_textures(const char *line, t_textures *textures)
+static void	free_split(char **split)
 {
-    if (ft_strncmp(line, "NO", 2) == 0)
-        textures->north = ft_strdup(line + 2); // Copia o caminho após "NO "
-    else if (ft_strncmp(line, "WE", 2) == 0)
-        textures->west = ft_strdup(line + 2); // Copia o caminho após "WE "
-    else if (ft_strncmp(line, "SO", 2) == 0)
-        textures->south = ft_strdup(line + 2); // Copia o caminho após "SO "
-    else if (ft_strncmp(line, "EA", 2) == 0)
-        textures->east = ft_strdup(line + 2); // Copia o caminho após "EA "
-    else
-    {
-        fprintf(stderr, "Error: Invalid texture line\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    //printf("passei aqui\n");
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
 }
 
-void parse_colors(const char *line, t_game *game)
-{    
-    char **split = ft_split(line + 1, ','); // Divide valores após "F " ou "C "
-    if (!split || !split[0] || !split[1] || !split[2])
-    {
-        fprintf(stderr, "Error: Invalid color format\n");
-        exit(EXIT_FAILURE);
-    }
+static int	get_color_value(char **split)
+{
+	int	r;
+	int	g;
+	int	b;
 
-    int r = ft_atoi(split[0]);
-    int g = ft_atoi(split[1]);
-    int b = ft_atoi(split[2]);
-
-    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-    {
-        fprintf(stderr, "Error: Color values must be between 0 and 255\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int color = (r << 16) | (g << 8) | b; // Converte para valor hexadecimal
-
-    if (line[0] == 'F')
-        game->floor_color = color;
-    else if (line[0] == 'C')
-        game->ceiling_color = color; // Corrige para usar o valor hexadecimal calculado
-
-    // Libera a memória
-    for (int i = 0; split[i]; i++)
-        free(split[i]);
-    free(split);
-    //printf("passei aqui nas cores\n");
+	r = ft_atoi(split[0]);
+	g = ft_atoi(split[1]);
+	b = ft_atoi(split[2]);
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+	{
+		fprintf(stderr, "Error: Color values must be between 0 and 255\n");
+		exit(EXIT_FAILURE);
+	}
+	return ((r << 16) | (g << 8) | b);
 }
 
-void load_images(void *mlx, t_textures *textures)
+void	load_textures(const char *line, t_textures *textures)
 {
-    int width, height;
-
-    textures->north = mlx_xpm_file_to_image(mlx, "asserts/textures/1.xpm", &width, &height);
-    
-    textures->south = mlx_xpm_file_to_image(mlx, "asserts/textures/2.xpm", &width, &height);
-
-    textures->east = mlx_xpm_file_to_image(mlx, "asserts/textures/3.xpm", &width, &height);
-
-    textures->west = mlx_xpm_file_to_image(mlx, "asserts/textures/4.xpm", &width, &height);
-
-if (!textures->north || !textures->south || !textures->east || !textures->west)
-{
-    fprintf(stderr, "Error: Failed to load one or more textures.\n");
-    exit(EXIT_FAILURE);
+	if (ft_strncmp(line, "NO", 2) == 0)
+		textures->north = ft_strdup(line + 2);
+	else if (ft_strncmp(line, "WE", 2) == 0)
+		textures->west = ft_strdup(line + 2);
+	else if (ft_strncmp(line, "SO", 2) == 0)
+		textures->south = ft_strdup(line + 2);
+	else if (ft_strncmp(line, "EA", 2) == 0)
+		textures->east = ft_strdup(line + 2);
+	else
+	{
+		fprintf(stderr, "Error: Invalid texture line\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-    debug_textures(textures);
+void	parse_colors(const char *line, t_game *game)
+{
+	char	**split;
+	int		color;
+
+	split = ft_split(line + 1, ',');
+	if (!split || !split[0] || !split[1] || !split[2])
+	{
+		fprintf(stderr, "Error: Invalid color format\n");
+		exit(EXIT_FAILURE);
+	}
+	color = get_color_value(split);
+	if (line[0] == 'F')
+		game->floor_color = color;
+	else if (line[0] == 'C')
+		game->ceiling_color = color;
+	free_split(split);
+}
+
+void	load_images(void *mlx, t_textures *textures)
+{
+	int	width;
+	int	height;
+
+	textures->north = mlx_xpm_file_to_image(mlx, "asserts/textures/1.xpm",
+			&width, &height);
+	textures->south = mlx_xpm_file_to_image(mlx, "asserts/textures/2.xpm",
+			&width, &height);
+	textures->east = mlx_xpm_file_to_image(mlx, "asserts/textures/3.xpm",
+			&width, &height);
+	textures->west = mlx_xpm_file_to_image(mlx, "asserts/textures/4.xpm",
+			&width, &height);
+	if (!textures->north || !textures->south || !textures->east || \
+		!textures->west)
+	{
+		fprintf(stderr, "Error: Failed to load one or more textures.\n");
+		exit(EXIT_FAILURE);
+	}
+	debug_textures(textures);
 }
--- utills/utils.c
+++ utills/utils.c
@@ -3,93 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tales <tales@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tlima-de <tlima-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:04:37 by tlima-de          #+#    #+#             */
-/*   Updated: 2025/02/18 14:01:00 by tales            ###   ########.fr       */
+/*   Updated: 2025/02/19 11:43:01 by tlima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-float distance(float x, float y)
+float	distance(float x, float y)
 {
-    return sqrt(x * x + y * y);
+	return (sqrt(x * x + y * y));
 }
 
-float fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
+float	fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
 {
-    float delta_x = x2 - x1;
-    float delta_y = y2 - y1;
-    float angle_diff = atan2(delta_y, delta_x) - game->player.angle;
+	float	delta_x;
+	float	delta_y;
+	float	angle_diff;
 
-    while (angle_diff < -PI)
-        angle_diff += 2 * PI;
-    while (angle_diff > PI)
-        angle_diff -= 2 * PI;
-
-    return sqrt(delta_x * delta_x + delta_y * delta_y) * cos(angle_diff);
+	delta_x = x2 - x1;
+	delta_y = y2 - y1;
+	angle_diff = atan2(delta_y, delta_x) - game->player.angle;
+	while (angle_diff < -PI)
+		angle_diff += 2 * PI;
+	while (angle_diff > PI)
+		angle_diff -= 2 * PI;
+	return (sqrt(delta_x * delta_x + delta_y * delta_y) * cos(angle_diff));
 }
 
-
-int touch(float px, float py, t_game *game)
+int	touch(float px, float py, t_game *game)
 {
-    if (!game || !game->map) // Ver se o game e o mapa são válidos ULTIMA COISA
-    {
-        //printf("error game - touch \n"); // tirar, botei pra teste todo if
-        return 0;
-    }
-        
-    int x = (int)(px / BLOCK);
-    int y = (int)(py / BLOCK);
+	int		x;
+	int		y;
+	float	offset_x;
+	float	offset_y;
 
-    if (y < 0 || y >= game->MAP_HEIGHT || x < 0 || x >= game->MAP_WIDTH)
-    {
-        //printf("error map - touch ok \n");  // tirar, botei pra teste todo if
-        return 0;
-    }
-
-    if (game->map[y][x] == '1')
-    {
-        float offset_x = px - x * BLOCK;
-        float offset_y = py - y * BLOCK;
-
-        if (offset_x < 1.0) // Perto da borda esquerda (WEST)
-            return WEST;
-        else if (offset_x > BLOCK - 1.0) // Perto da borda direita (EAST)
-            return EAST;
-        else if (offset_y < 1.0) // Perto da borda superior (NORTH)
-            return NORTH;
-        else if (offset_y > BLOCK - 1.0) // Perto da borda inferior (SOUTH)
-            return SOUTH;
-    }
-    return 0; // Sem colisão
+	if (!game || !game->map)
+		return (0);
+	x = (int)(px / BLOCK);
+	y = (int)(py / BLOCK);
+	if (y < 0 || y >= game->MAP_HEIGHT || x < 0 || x >= game->MAP_WIDTH)
+		return (0);
+	if (game->map[y][x] == '1')
+	{
+		offset_x = px - x * BLOCK;
+		offset_y = py - y * BLOCK;
+		if (offset_x < 1.0)
+			return (WEST);
+		else if (offset_x > BLOCK - 1.0)
+			return (EAST);
+		else if (offset_y < 1.0)
+			return (NORTH);
+		else if (offset_y > BLOCK - 1.0)
+			return (SOUTH);
+	}
+	return (0);
 }
 
-void debug_textures(t_textures *textures)
+void	debug_textures(t_textures *textures)
 {
-    if (textures->north)
-        printf("North texture loaded\n");
-    if (textures->south)
-        printf("South texture loaded\n");
-    if (textures->east)
-        printf("East texture loaded\n");
-    if (textures->west)
-        printf("West texture loaded\n");
+	if (textures->north)
+		printf("North texture loaded\n");
+	if (textures->south)
+		printf("South texture loaded\n");
+	if (textures->east)
+		printf("East texture loaded\n");
+	if (textures->west)
+		printf("West texture loaded\n");
 }
 
-
-void free_split(char **split)
+void	free_split(char **split)
 {
-    int i = 0;
+	int	i;
 
-    if (!split)
-        return;
-
-    while (split[i])
-    {
-        free(split[i]);
-        i++;
-    }
-    free(split);
+	i = 0;
+	if (!split)
+		return ;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
 }
